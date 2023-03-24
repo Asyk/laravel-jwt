225f0ccccbf946a9c9fdda82101a16a2b9f60e2f
AssyQ <asyk7kz@gmail.com>
2023-03-24 19:46:38 +0600

    JWT add

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+
+Route::prefix('auth')
+    ->middleware('api')
+    ->controller(AuthController::class)
+    ->group(function () {
+        Route::get('me', 'me');
+        Route::post('login', 'login');
+        Route::post('refresh', 'refresh');
+        Route::post('logout', 'logout');
+    });
